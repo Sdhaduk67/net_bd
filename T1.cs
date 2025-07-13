d183dfb648305b228d54baf3229246fc72382a0e
mahadev <mahadev@DESKTOP-192B6UQ>
2025-07-13 20:43:29 +0530

    commit

---
+++ T1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_bd
+{
+    internal class T1
+    {
+    public static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ T2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_bd
+{
+    internal class T2
+    {
+        public static void Main (string[] args)
+
+        {
+            Console.WriteLine("Name : Shreya Dhaduk");
+            Console.WriteLine("DOB: 21/05/2006");
+            Console.WriteLine("Address: Ghoghavadar,Ramod Road");
+            Console.WriteLine("City: Ghoghavadar");
+            Console.WriteLine("Pincode: 360311");
+            Console.WriteLine("State: Gujarat");
+            Console.WriteLine("Country: India");
+            Console.WriteLine("Email: shreyadhaduk123@gmail.com");
+        }
+    }
+}
